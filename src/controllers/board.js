84fb48a0a548fc11e4075d595b5a27e16a5ab930
Sergey Ivashko <grayseneov@gmail.com>
2019-05-27 19:31:43 +0300

    udpate board

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -50,4 +50,45 @@ const getBoard = async (req, res) => {
   }
 };
 
-export { getBoards, getBoard };
+const editBoard = async (req, res) => {
+  try {
+    const { params: { id }, body: { name, description, columns } } = req;
+
+    await prisma.updateBoard({ data: { name, description }, where: { id } });
+
+    const boardColumns = await prisma.columns({ where: { board: { id } } });
+    const needDelete = boardColumns
+      .filter(col => !columns.some(c => c.id === col.id))
+      .map(col => col.id);
+    await prisma.deleteManyColumns({ id_in: needDelete });
+    await prisma.deleteManyCards({ column: { id_in: needDelete } });
+    await Promise.all(columns.map(async (col) => {
+      await prisma.upsertColumn({
+        where: {
+          id: col.id || '',
+        },
+        update: {
+          name: col.name,
+        },
+        create: {
+          name: col.name,
+          board: {
+            connect: {
+              id,
+            },
+          },
+        },
+      });
+    }));
+
+    const board = await prisma.board({ id }).$fragment(fragment);
+    res.status(200).json(board);
+  } catch (e) {
+    res.status(422).json({
+      error: e.message,
+      raw: e,
+    });
+  }
+};
+
+export { getBoards, getBoard, editBoard };
--- src/routes/board.js
+++ src/routes/board.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getBoards, getBoard } from 'controllers/board';
+import { getBoards, getBoard, editBoard } from 'controllers/board';
 
 const boardRouter = express.Router();
 
 boardRouter.get('/', getBoards);
 boardRouter.get('/:id', getBoard);
+boardRouter.patch('/:id', editBoard);
 
 export default boardRouter;
